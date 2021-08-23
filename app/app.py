4403177226c4e3fc13a051130f08715bb7604034
Михаил Ященко <chemist138@mail.ru>
2021-08-23 16:26:50 +0300

    added analogue of REST API, so now you can use curl (for example curl -i http://localhost:5000/api/v1.0/employees)

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
 import psycopg2
 import json
+from flask import Flask, jsonify
+from flask import make_response
 
 #connect to the db
 con = psycopg2.connect(host="db",
@@ -11,12 +13,12 @@ con = psycopg2.connect(host="db",
 #cursor 
 cur = con.cursor()
 
-cur.execute("""CREATE TABLE employees
-            (
-            UserID int,
-            name varchar(100)
-            );"""
-)
+# cur.execute("""CREATE TABLE employees
+#             (
+#             UserID int,
+#             name varchar(100)
+#             );"""
+# )
 cur.execute("insert into employees (UserID, name) values (1, 'Teacher Paul')")
 con.commit()
 
@@ -33,9 +35,27 @@ for r in rows:
 #         f.write(f"\{ {r[0]}: {r[1]} \}")
 #         print(f"UserID {r[0]} name {r[1]}")
 
+print(data)
+
 with open('data.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, indent=4)
 f.close()
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+@app.route('/api/v1.0/employees', methods=['GET'])
+def get_employee():
+    return jsonify({'employee': data})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+app.run(host='0.0.0.0')
 
 cur.close()
 #close the connection
